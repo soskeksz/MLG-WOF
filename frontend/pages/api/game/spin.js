85eca02a0f4350b7b0ed1ad48d154b7604e4b17b
Zoltai Barna <soskeksz115@gmail.com>
2025-05-19 18:22:42 +0200

    update spin api for additional outcomes

--- frontend/pages/api/game/spin.js
+++ frontend/pages/api/game/spin.js
@@ -1,16 +1,26 @@
 import connectToDatabase from '../../../lib/mongodb';
 import User from '../../../lib/models/User';
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // Game logic function
 function getSpinResult() {
   const random = Math.random();
   
-  if (random < 0.1) {
-    return { result: "DOUBLE", angle: 30 };
-  } else if (random < 0.7) {
-    return { result: "KEEP", angle: 150 };
+  if (random < 0.05) {
+    return { result: "BANKRUPT", angle: getRandomInt(0, 18) };
+  } else if (random > 0.05 && random < 0.35) {
+    return { result: "LOSE", angle: getRandomInt(18, 126) };
+  } else if (random > 0.35 && random < 0.65) {
+    return { result: "KEEP", angle: getRandomInt(126, 234) };
+  } else if (random > 0.65 && random < 0.85) {
+    return { result: "TRIPLE", angle: getRandomInt(234, 306) };
+  } else if (random > 0.85 && random < 0.95) {
+    return { result: "THOMAS", angle: getRandomInt(306, 342) };         
   } else {
-    return { result: "BANKRUPT", angle: 270 };
+    return { result: "JACKPOT", angle: getRandomInt(342, 360) };
   }
 }
 
@@ -48,15 +58,24 @@ export default async function handler(req, res) {
       
       // Process the result
       switch (result) {
-        case "DOUBLE":
-          newBalance += betAmount;
+        case "JACKPOT":
+          newBalance += betAmount * 42;
+          break;
+        case "THOMAS":
+          newBalance += betAmount * 6.9;
+          break;
+        case "TRIPLE":
+          newBalance += betAmount * 3;
           break;
         case "KEEP":
           // Balance stays the same
           break;
+        case "LOSE":
+          newBalance -= betAmount;
+          break;
         case "BANKRUPT":
           newBalance = 0;
-          break;
+          break;    
       }
       
       // Update user's money
